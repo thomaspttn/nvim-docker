9c871c585503dd4450f9ff84fa48a559fe74992c
Thomas Patton <tjpatton1@gmail.com>
2024-01-03 17:45:08 -0500

    init

---
+++ install.sh
@@ -0,0 +1,40 @@
+# FILE: install.sh
+# DESCRIPTION: Master setup script for nvim-docker
+# AUTHOR: Thomas Patton
+
+# install directory
+INSTALL_DIR="/root/"
+
+if [ -f "/proc/1/cgroup" ]; then
+    echo "Running Docker container setup..."
+
+    cd "$INSTALL_DIR"
+    rm -rf "${INSTALL_DIR}.config"
+    sudo apt-get update
+
+    # CLANG
+    sudo apt-get install -y clang
+    sudo ln -s -f .clangd "~/.clangd"
+
+    # BEAR
+    sudo apt-get install -y bear
+
+    # NEOVIM
+    wget https://github.com/neovim/neovim/releases/download/v0.9.5/nvim-linux64.tar.gz
+    tar xzvf nvim-linux64.tar.gz
+    sudo ln -s "${INSTALL_DIR}nvim-linux64/bin/nvim" /usr/local/bin/nvim
+
+    # RIPGREP
+    curl -LO https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb
+    sudo apt-get install "./ripgrep_13.0.0_amd64.deb"
+
+    # CHADNV
+    git clone https://github.com/NvChad/NvChad "${INSTALL_DIR}.config/nvim" --depth 1 && nvim
+
+    # Update XDG_CONFIG_HOME
+    export XDG_CONFIG_HOME="${INSTALL_DIR}.config"
+
+else
+    echo "This doesn't seem to be a Docker container... are you sure??"
+fi
+
