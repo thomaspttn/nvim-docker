c650ee3cd4ebd0d50d91fb33f873d936c913598b
Thomas Patton <tjpatton1@gmail.com>
2024-01-03 17:49:14 -0500

    updates

---
+++ custom/init.lua
@@ -0,0 +1,16 @@
+-- local autocmd = vim.api.nvim_create_autocmd
+
+-- Auto resize panes when resizing nvim window
+-- autocmd("VimResized", {
+--   pattern = "*",
+--   command = "tabdo wincmd =",
+-- })
+-- -- Set tab width to 4 spaces
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+
+-- Use spaces instead of tabs
+vim.o.expandtab = true
+
+vim.o.termguicolors = true
